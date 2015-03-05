c344ff8875b42a7ec646c0e0bdcbab2fe0017c8a
Durga Prasad <dprasad_c@rpxcorp.com>
2015-03-05 14:51:34 +0530

    brn style changed.

--- js/caluculator.js
+++ js/caluculator.js
@@ -1,36 +1,85 @@
-$( document ).ready(function() {
-	$('.num_sym').click(function(){
-	  var screenVal = $("#screen").val();
-	  var currentChar = $(this).text();
-	  if(screenVal != "Infinity" && screenVal != "Error"){
-	  	var lastChar = screenVal.substr(-1);
-	  	if(/^[0-9]*$/.test(lastChar) == false){
-	  		if(/^[0-9]*$/.test(currentChar) == true )
-	        screenVal = screenVal+currentChar;
-	  	}
-	  	else
-	  	  screenVal = screenVal+currentChar;
-	  }
-    else{
-      screenVal = currentChar;
+var calculator = {
+    '+': function(a, b) {
+        return a + b;
+    },
+
+    '-': function(a, b) {
+        return a - b;
+    },
+
+    '*': function(a, b) {
+        return a * b;
+    },
+
+    '/': function(a, b) {
+        return a / b;
+    },
+
+    '%': function(a, b) {
+        return a / b;
+    },
+
+    calc: function(a, b, op) {
+        if (this[op] === undefined) {
+            console.log("Unknown operation!!");
+            return undefined;
+        }
+        return this[op](parseFloat(a), parseFloat(b));
     }
-    $("#screen").val(screenVal);
-	});
+};
 
-	$('#reset').click(function(){
-     $("#screen").val("");
-	});
+function inputString(inputVal){
+	var screen = document.getElementById("screen");
+  var screenVal = screen.value
+  var currentChar = inputVal;
+  if(screenVal != "Infinity" && screenVal != "Error" && screenVal != "NaN"){
+  	var lastChar = screenVal.substr(-1);
+  	if(/^[0-9]*$/.test(lastChar) == false){
+  		if(/^[0-9]*$/.test(currentChar) == true )
+        screenVal = screenVal+currentChar;
+  	}
+  	else
+  	  screenVal = screenVal+currentChar;
+  }
+  else{
+    screenVal = currentChar;
+  }
+  screen.value = screenVal;
+}
 
-	$('#result').click(function(){
-		try
-		{
-		  screenVal = eval($("#screen").val());
-      $("#screen").val(screenVal);
-		}
-		catch(e)
-		{
-		  $("#screen").val('Error');
-		}
-	});
-});
+function reset(){
+  document.getElementById("screen").value="";
+}
 
+function result(){
+	var screen = document.getElementById("screen");
+	var regex = /[+,-,*,%,\/,-]/gi, indices = [], result="";
+	var screenVal = screen.value;
+	try
+	{
+		while ( (searchPos = regex.exec(screenVal)) ) {
+			if(searchPos.index != 0)
+		    indices.push(searchPos.index);
+    }
+
+    for (i = 0; i < indices.length; i += 1) {
+    	symb = screenVal.charAt(indices[i])
+      if(result==""){
+        result = screenVal.substring(0, indices[i]);
+      }
+      next = screenVal.substring(indices[i]+1, indices[i+1]);
+      console.log(result)
+      console.log(next)
+      console.log(symb)
+      result = calculator.calc(result, next, symb);
+      console.log(result)
+      console.log("-----------")
+    }
+
+    screen.value = parseFloat(result);
+	}
+	catch(e)
+	{
+	  screen.value = 'Error';
+	}
+}
